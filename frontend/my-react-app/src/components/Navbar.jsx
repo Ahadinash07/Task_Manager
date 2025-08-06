e7b3e0b112861edd6e40a88ee630dbac2cd86a63
Yogesh Pandey <ypandey953@gmail.com>
2025-08-06 08:54:49 +0530

    First commit

--- frontend/my-react-app/src/components/Navbar.jsx
+++ frontend/my-react-app/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Task Manager
+          Manage Your Task
         </Typography>
         {user ? (
           <>
